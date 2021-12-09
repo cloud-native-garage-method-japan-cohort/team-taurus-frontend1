1a7014ce8ae700a2d603db6d4c2b910467b2bea0
Tokinori Shimodaira <Tokinori.Shimodaira@ibm.com>
2021-12-10 03:27:37 +0900

    chg config

--- webapp/k8s/pipeline/create_secret.sh
+++ webapp/k8s/pipeline/create_secret.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export CONTAINER_REGISTRY_SERVER='https://quay.io'
-export CONTAINER_REGISTRY_USER='yu_yamanaka_ibm'
-export CONTAINER_REGISTRY_PASSWORD='SwSvqex0sytn9zxEa+R7o5TcMVmQjHZTrbS+x+3VFZ3lJo/+ktR5iT3+ozx0ePz7'
-kubectl create secret -n team-gemini docker-registry quay-io-team-gemini-password --docker-server=$CONTAINER_REGISTRY_SERVER --docker-username=$CONTAINER_REGISTRY_USER --docker-password=$CONTAINER_REGISTRY_PASSWORD
+export CONTAINER_REGISTRY_USER='tokinori_shimodaira_ibm'
+export CONTAINER_REGISTRY_PASSWORD='03OZdz48DC7hwxRfUwsMhZqeVqCGcEFDlK9PWpnzdJE2DxFO2vOYg5zkjibt1qxffQMMbjyJC22QZWx/ub4yzAW5Vd7UU14Lb8og70cB1AM='
+kubectl create secret -n team-taurus1 docker-registry quay-io-team-taurus1-password --docker-server=$CONTAINER_REGISTRY_SERVER --docker-username=$CONTAINER_REGISTRY_USER --docker-password=$CONTAINER_REGISTRY_PASSWORD
 
